d83d3b0000a48b608d10db73d2239e8b79577883
SKY3E <bessin.raphael@gmail.com>
2023-05-28 11:11:06 -0400

    Added Responsive Styling

--- app/components/about.js
+++ app/components/about.js
@@ -1,43 +1,51 @@
+'use client';
+
+import { useMediaQuery } from '@react-hook/media-query';
+import { useState, useEffect } from 'react';
+
 export default function About() {
+  const isXLargeScreen = useMediaQuery('(min-width: 1280px)');
+  
   return (
-    <section id='about-section' className='-mt-60 ml-[calc(60.00vw-50.00%)] h-vh-screen'>
+    <section id='about-section' className='-mt-60 lg:ml-[calc(60.00vw-50.00%)] lg:mr-[calc(60.00vw-50.00%)] h-vh-screen'>
       <h2 className='text-2xl font-semibold mb-4'>About Me</h2>
-      <article className="lg:w-6/12 font-semibold">
-        <p className="mb-4">
-          I first started coding in 2021, at 14 years old. I started with a python course. However, I quickly moved on to work on my own projects, and started learning Unity & C# to make games. A bit later I decided to go into Web Development, and since then I've worked on over 10 projects, learned various languages, and decided to pursue Computer Science as a career.
-        </p>
-        <div className="mb-4 flex space-x-2">
-          <div className="w-1/2 h-36 px-4 py-2 bg-gray-100 border-gray-200 border-2 rounded">
-            Natural Languages : <br />
-            <ul className="list-disc list-inside">
-              <li>French</li>
-              <li>English</li>
-              <li>German (Limited Proficiency)</li>
-            </ul>
+      <div className="xl:flex">
+        <article className="xl:w-6/12 font-semibold">
+          <p className="mb-4">
+            I first started coding in 2021, at 14 years old. I started with a python course. However, I quickly moved on to work on my own projects, and started learning Unity & C# to make games. A bit later I decided to go into Web Development, and since then I've worked on over 10 projects, learned various languages, and decided to pursue Computer Science as a career.
+          </p>
+          <div className="mb-4 flex space-x-2">
+            <div className="w-1/2 h-36 px-4 py-2 bg-gray-100 border-gray-200 border-2 rounded">
+              Natural Languages: <br />
+              <ul className="list-disc list-inside">
+                <li>French</li>
+                <li>English</li>
+                <li>German (Limited Proficiency)</li>
+              </ul>
+            </div>
+            <div className="w-1/2 h-36 px-4 py-2 bg-gray-100 border-gray-200 border-2 rounded">
+              Programming Languages: <br />
+              <ul className="list-disc list-inside">
+                <li>Javascript</li>
+                <li>Python</li>
+                <li>C#</li>
+              </ul>
+            </div>
           </div>
-          <div className="w-1/2 h-36 px-4 py-2 bg-gray-100 border-gray-200 border-2 rounded">
-            Programming Languages : <br />
+          <div className="w-full h-36 px-4 py-2 bg-gray-100 border-gray-200 border-2 rounded">
+            Personal Tech Stack: <br />
             <ul className="list-disc list-inside">
+              <li>Next.js</li>
               <li>Javascript</li>
-              <li>HTML / (CSS + Tailwind)</li>
-              <li>Python</li>
-              <li>C#</li>
+              <li>Firebase -&gt; Postgres</li>
+              <li>Tailwind</li>
             </ul>
           </div>
-        </div>
-        <div className="w-full h-36 px-4 py-2 bg-gray-100 border-gray-200 border-2 rounded">
-          Personal Tech Stack : <br />
-          <ul className="list-disc list-inside">
-            <li>Next.js</li>
-            <li>Javascript</li>
-            <li>Firebase -&gt; Postgres</li>
-            <li>Tailwind</li>
-          </ul>
-        </div>
-      </article>
-      <article className="lg:w-6/12 font-semibold">
-        
-      </article>
+        </article>
+        {isXLargeScreen && <article className="xl:w-6/12 font-semibold flex justify-center items-center">
+          <img src="ProfilePic.jpg" alt="Profile Picture" className="h-5/6 rounded-full border-gray-200 border-4 rounded" />
+        </article>}
+      </div>
     </section>
   )
 }
--- app/components/header.js
+++ app/components/header.js
@@ -1,11 +1,11 @@
 export default function Header(){
 
   return (
-    <section id='header-section' className='mt-60 ml-[calc(60.00vw-50.00%)] h-vh-screen'>
+    <section id='header-section' className='mt-60 lg:ml-[calc(60.00vw-50.00%)] lg:mr-[calc(60.00vw-50.00%)] h-vh-screen'>
       <h2 className='text-xl font-semibold mb-4'>Hi, my name is</h2>
-      <h1 className='text-5xl font-bold mb-2'>Raphael Bessin</h1>
+      <h1 className='text-5xl font-bold mb-2'>Raphael Bessin.</h1>
       <h1 className='text-5xl font-bold mb-4'>I develop the future of the Web.</h1>
-      <h3 className='w-3/12 font-semibold mb-4'>I'm a sophomore in highschool looking to pursue a Software Development career in college by getting a Computer Science degree. I'm also currently involved in a robotics club and hope to participate in hackathons.</h3>
+      <h3 className='xl:w-1/2 font-semibold mb-4'>I'm a sophomore in highschool looking to pursue a Software Development career in college by getting a Computer Science degree. I'm also currently involved in a robotics club and hope to participate in hackathons.</h3>
       <button className='bg-gray-100 border-gray-200 border-2 rounded px-4 py-2'>Learn More</button>
     </section>
   )
