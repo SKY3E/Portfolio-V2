a0e8e6b0ab79c43c01d4ad6b4c74b80428d4522e
SKY3E <bessin.raphael@gmail.com>
2023-05-28 08:28:45 -0400

    Implemented Header & Updated Styling

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,17 +1,22 @@
-'use client';
+export default function Navigation({ handleScroll }) {
 
-import { useState } from 'react';
-import Link from "next/link";
+  const handleScrollClick = (e) => {
+    handleScroll(e.target.id);
+  };
 
-export default function Navigation() {
-  
   return (
     <nav className="fixed z-10 top-0 left-0 right-0 h-16 bg-gray-100 border-gray-200 border-2 rounded flex items-center justify-between text-gray-200 p-4 m-2">
-      <Link href={'/'}>
-        <button className="flex items-center justify-center w-10 h-10 rounded-full border-gray-200 border-2 bg-white font-extrabold">
-          R
+      <button onClick={handleScrollClick} id='header' className="flex items-center justify-center w-10 h-10 rounded-full border-gray-200 border-2 bg-white font-extrabold">
+        R
+      </button>
+      <section className="flex space-x-2">
+        <button onClick={handleScrollClick} id='about' className="flex items-center justify-center px-6 py-2 rounded border-gray-200 border-2 bg-white font-extrabold">
+          About
         </button>
-      </Link>
+        <button onClick={handleScrollClick} id='projects' className="flex items-center justify-center px-6 py-2 rounded border-gray-200 border-2 bg-white font-extrabold">
+          Projects
+        </button>
+      </section>
     </nav>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,16 +1,31 @@
+'use client';
+
 import Navigation from './components/navigation.js';
+import Header from './components/header.js';
+
+import { useRef } from 'react';
 
 export default function Home() {
+  const handleScroll = (id) => {
+    console.log(id);
+    const targetElement = document.getElementById(id + '-section');
+    if (targetElement) {
+      console.log(targetElement);
+      const targetPosition = targetElement.getBoundingClientRect().top;
+      console.log(targetPosition);
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
-    <main className=''>
-      <Navigation />
-      <section className='mt-60 ml-[calc(60.00vw-50.00%)]'>
-        <h2 className='text-xl font-semibold mb-4'>Hi, my name is</h2>
-        <h1 className='text-5xl font-bold mb-2'>Raphael Bessin</h1>
-        <h1 className='text-5xl font-bold mb-4'>I develop the future of the Web.</h1>
-        <h3 className='w-3/12 font-semibold mb-4'>I'm a sophomore in highschool looking to pursue a Software Development career in college by getting a Computer Science degree. I'm also currently involved in a robotics club and hope to participate in hackathons.</h3>
-        <button className='bg-gray-100 border-gray-200 border-2 rounded px-4 py-2'>Learn More</button>
-      </section>
+    <main className='h-96'>
+      <Navigation handleScroll={handleScroll}/>
+      <Header />
+      <h1 className='mt-96 mb-96'>Hello</h1>
+      <h1 className='mt-96 mb-96'>Hello</h1>
     </main>
   )
 }
