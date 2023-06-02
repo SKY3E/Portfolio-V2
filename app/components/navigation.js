6bbdd15c406081900013d062b58022628b26274d
SKY3E <bessin.raphael@gmail.com>
2023-06-02 13:32:13 -0400

    Updated Navigation Styling

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -5,19 +5,22 @@ export default function Navigation({ handleScroll }) {
   };
 
   return (
-    <nav className="fixed z-10 top-0 left-0 right-0 h-16 bg-gray-100 border-gray-200 border-2 rounded flex items-center justify-between shadow-lg p-4 m-2">
-      <button onClick={handleScrollClick} id='header' className="flex items-center justify-center w-10 h-10 rounded-full border-gray-200 border-2 bg-white font-extrabold">
+    <nav className="fixed z-10 top-0 left-0 right-0 h-16 bg-white border-gray-200 border-2 rounded flex items-center justify-between shadow-lg p-4 m-2">
+      <button onClick={handleScrollClick} id='header' className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-100 hover:shadow-lg border-gray-200 border-2 px-4 py-2">
         R
       </button>
       <section className="flex space-x-2">
-        <button onClick={handleScrollClick} id='about' className="flex items-center justify-center px-6 py-2 rounded border-gray-200 border-2 bg-white font-extrabold">
+        <button onClick={handleScrollClick} id='about' className="flex items-center justify-center bg-gray-100 hover:shadow-lg border-gray-200 border-2 rounded px-4 py-2">
           About
         </button>
-        <button onClick={handleScrollClick} id='projects' className="flex items-center justify-center px-6 py-2 rounded border-gray-200 border-2 bg-white font-extrabold">
+        <button onClick={handleScrollClick} id='projects' className="flex items-center justify-center bg-gray-100 hover:shadow-lg border-gray-200 border-2 rounded px-4 py-2">
           Projects
         </button>
-        <button onClick={handleScrollClick} id='skills' className="flex items-center justify-center px-6 py-2 rounded border-gray-200 border-2 bg-white font-extrabold">
+        <button onClick={handleScrollClick} id='skills' className="flex items-center justify-center bg-gray-100 hover:shadow-lg border-gray-200 border-2 rounded px-4 py-2">
           Skills
+        </button>
+        <button onClick={handleScrollClick} id='contact' className="flex items-center justify-center bg-gray-100 hover:shadow-lg border-gray-200 border-2 rounded px-4 py-2">
+          Contact
         </button>
       </section>
     </nav>
