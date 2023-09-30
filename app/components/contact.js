a8801306dc3e1022f385b13a31e9f7a656614de0
SKY3E <bessin.raphael@gmail.com>
2023-09-30 12:57:55 -0400

    Updated Contact Page

--- app/components/contact.js
+++ app/components/contact.js
@@ -25,24 +25,32 @@ export default function Contact() {
   return (
     <section id='contact-section' className='lg:ml-[calc(60.00vw-50.00%)] lg:mr-[calc(60.00vw-50.00%)]'>
       <h2 className='text-2xl font-semibold mb-4'>Contact Me</h2>
-      <form className='flex flex-col' onSubmit={handleSubmit}>
-        <div className="flex w-1/2 mb-2 gap-2">
-          <label className='rounded bg-gray-200 border-gray-300 border-2 w-1/3 px-4 py-2'>First & Last Name</label>
-          <div className='rounded bg-gray-200 border-gray-300 border-2 w-2/3 px-4 py-2'></div>
+      <article className="flex w-full gap-16">
+        <form className='flex flex-col w-1/2' onSubmit={handleSubmit}>
+          <div className="flex mb-2 gap-2">
+            <label className='rounded bg-gray-200 border-gray-300 border-2 w-1/3 px-4 py-2'>First & Last Name</label>
+            <div className='rounded bg-gray-200 border-gray-300 border-2 w-2/3 px-4 py-2'></div>
+          </div>
+          <input className='rounded bg-gray-100 border-gray-200 border-2 px-4 py-2 mb-2' type="text" name="name" placeholder="John Doe"/>
+          <div className="flex mb-2 gap-2">
+            <label className='rounded bg-gray-200 border-gray-300 border-2 w-1/3 px-4 py-2'>Email</label>
+            <div className='rounded bg-gray-200 border-gray-300 border-2 w-2/3 px-4 py-2'></div>
+          </div>
+          <input className='rounded bg-gray-100 border-gray-200 border-2 px-4 py-2 mb-2' type="email" name="email" placeholder="johndoe@gmail.com"/>
+          <div className="flex mb-2 gap-2">
+            <label className='rounded bg-gray-200 border-gray-300 border-2 w-1/3 px-4 py-2'>Message</label>
+            <div className='rounded bg-gray-200 border-gray-300 border-2 w-2/3 px-4 py-2'></div>
+          </div>
+          <textarea className='rounded bg-gray-100 border-gray-200 border-2 px-4 py-2 mb-2' name="message" placeholder="Type your message here."></textarea>
+          <button className='rounded text-white bg-green-700 border-green-800 border-2 hover:bg-green-600 hover:border-green-700 px-4 py-2 mb-2' type="submit">Submit Form</button>
+        </form>
+        <div className="w-1/2">
+          <h2 className="text-3xl font-bold mb-1">Got a question?</h2>
+          <h3 className="text-3xl font-semibold mt-1">Just reach out.</h3>
+          <p className="mt-4">Contact me through the form whether it be to work on a project today or if you have a question.</p>
+          <p className="mt-2">Or just contact me directly through my <a className='underline' href="mailto:bessin.raphael@gmail.com">email</a>.</p>
         </div>
-        <input className='rounded bg-gray-100 border-gray-200 border-2 px-4 py-2 w-1/2 mb-2' type="text" name="name" placeholder="John Doe"/>
-        <div className="flex w-1/2 mb-2 gap-2">
-          <label className='rounded bg-gray-200 border-gray-300 border-2 w-1/3 px-4 py-2'>Email</label>
-          <div className='rounded bg-gray-200 border-gray-300 border-2 w-2/3 px-4 py-2'></div>
-        </div>
-        <input className='rounded bg-gray-100 border-gray-200 border-2 px-4 py-2 w-1/2 mb-2' type="email" name="email" placeholder="johndoe@gmail.com"/>
-        <div className="flex w-1/2 mb-2 gap-2">
-          <label className='rounded bg-gray-200 border-gray-300 border-2 w-1/3 px-4 py-2'>Message</label>
-          <div className='rounded bg-gray-200 border-gray-300 border-2 w-2/3 px-4 py-2'></div>
-        </div>
-        <textarea className='rounded bg-gray-100 border-gray-200 border-2 px-4 py-2 w-1/2 mb-2' name="message" placeholder="Type your message here."></textarea>
-        <button className='rounded text-white bg-green-700 border-green-800 border-2 hover:bg-green-600 hover:border-green-700 px-4 py-2 w-1/2 mb-2' type="submit">Submit Form</button>
-      </form>
+      </article>
     </section>
   )
 }
